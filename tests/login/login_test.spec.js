0c3c6b2ce6a48d9f489df82a5714976783517e54
tracy8 <tracymurenzi@gmail.com>
2024-04-12 13:26:45 +0200

    password-reset

--- tests/login/login_test.spec.js
+++ tests/login/login_test.spec.js
@@ -5,9 +5,9 @@ test ('login test', async ({page}) => {
 
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     await page.pause()
-    await page.getByPlaceholder('Username').click();
+    // await page.getByPlaceholder('Username').fill('Tracy');
     await page.getByPlaceholder('Username').fill('Admin');
-    await page.getByPlaceholder('Password').click();
+    // await page.getByPlaceholder('Password').fill('123');
     await page.getByPlaceholder('Password').fill('admin123');
     await page.getByRole('button', { name: 'Login' }).click();
 
