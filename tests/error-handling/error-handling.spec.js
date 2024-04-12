b165449fc7eb7e761f36dc6160b28940d3e994ab
tracy8 <tracymurenzi@gmail.com>
2024-04-12 20:13:08 +0200

    fix

--- tests/error-handling/error-handling.spec.js
+++ tests/error-handling/error-handling.spec.js
@@ -7,7 +7,7 @@ test ('error handling', async ({page}) => {
     await page.pause()
     //check element present or not
     await expect (page.locator('text=The Kitchen')).toBeVisible()
-    await expect.soft (page.locator('text=The Kitchen')).toBeHidden()
+    // await expect.soft (page.locator('text=The Kitchen')).toBeHidden()
     await expect (page.locator('text=The Kitchen')).toHaveCount(1)
     
     })
